073e29c9bed2958cdea436b86d3b8b7445412892
adem rb <adamrabia79@gmail.com>
2024-02-04 14:25:16 +0100

    The Codes

---
+++ main.js
@@ -0,0 +1,36 @@
+let pass = document.getElementById("pass");
+let btn = document.getElementById("btn");
+let btncp = document.getElementById("btncp");
+
+function generatePass() {
+  let chars =
+    "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}?><";
+  let passLength = 8;
+  let password = "";
+  for (let i = 0; i < passLength; i++)
+    password += chars[Math.floor(Math.random() * chars.length)];
+
+  return password;
+  writeText;
+}
+btn.addEventListener("click", () => {
+  pass.value = generatePass();
+});
+
+btncp.addEventListener("click", () => {
+  if (pass.value != "") {
+    navigator.clipboard.writeText(pass.value);
+    pass.value = "Copied!";
+    pass.classList.add("active");
+    setTimeout(() => {
+      pass.value = "";
+      pass.classList.remove("active");
+    }, 1000);
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please Press On Generate Password !!",
+    });
+  }
+});
